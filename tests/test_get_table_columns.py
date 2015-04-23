ff3ed04e05832e0beb0fa9e3d68636676d785180
Paul Craciunoiu <paul@craciunoiu.net>
2015-04-23 11:11:03 -0700

    Don't mark xfail

--- tests/test_get_table_columns.py
+++ tests/test_get_table_columns.py
@@ -4,13 +4,13 @@ import pytest
 import cc_dynamodb
 
 @mock_dynamodb
-@pytest.mark.xfail(raises=cc_dynamodb.UnknownTableException)
 def test_get_dynamodb_table_unknown_table_raises_exception(fake_config):
-    cc_dynamodb.get_table('invalid_table')
+    with pytest.raises(cc_dynamodb.UnknownTableException):
+        cc_dynamodb.get_table('invalid_table')
 
-@pytest.mark.xfail(raises=cc_dynamodb.UnknownTableException)
 def test_get_dynamodb_table_unknown_table_raises_exception(fake_config):
-    cc_dynamodb.get_table_columns('invalid_table')
+    with pytest.raises(cc_dynamodb.UnknownTableException):
+        cc_dynamodb.get_table_columns('invalid_table')
 
 def test_get_dynamodb_table_columns_should_return_columns(fake_config):
     columns = cc_dynamodb.get_table_columns('telephony_call_logs')
