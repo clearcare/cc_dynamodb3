b8a153b08cc755ba5e5e0774636812059288d629
Derrick Petzold <github@bzero.io>
2016-06-02 14:49:26 -0700

    Bumped major version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'schematics==1.1.1',
     ],
     tests_require=['pytest', 'mock', 'factory_boy', 'moto'],
-    version = '0.6.14',
+    version = '1.0.0',
     description = 'A dynamodb common configuration abstraction',
     author='Paul Craciunoiu',
     author_email='pcraciunoiu@clearcareonline.com',
